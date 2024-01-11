098c54f96e93262055284c53943495befd956390
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-01-11 02:29:26 +0100

    adding notes on UI

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,6 +1,24 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "title1",
+      content: "content 1",
+    },
+    {
+      id: 2,
+      title: "title2",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "title3",
+      content: "content 3",
+    },
+  ]);
+
   return (
     <div className="app-container">
       <form className="note-form">
@@ -9,13 +27,15 @@ function App() {
         <button type="submit">Add Note </button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Title</h2>
-          <p>Note content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
